96d2142d20d6d5ed8f3cfdccd56502323a4ea208
Rebecca <hrmrebecca@gmail.com>
2012-10-25 21:19:36 -0400

    initial commit

---
+++ app/models/person.rb
@@ -0,0 +1,3 @@
+class Person < ActiveRecord::Base
+  attr_accessible :ID, :name1, :name2 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testprogram::Application.config.secret_token = 'a0b6f49c41f9ff71db13fc5bf176c0cfb4a30e90641a5fdebd2c666f01e20ec6dca8da04c5f66041ca9cde2ee4c385afa860e2cc4601555b1b608e38a930892f'
